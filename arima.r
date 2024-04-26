82b38497700080b4123275ffdbde1466981e0ce2
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2024-04-26 17:30:11 +0530

    Update arima.r

--- arima.r
+++ arima.r
@@ -1,13 +1,8 @@
-# r-studio mei run karna saare commands
-
-install.packages("forecast")
-library(forecast)
-
 data("AirPassengers")
-passengers_ts <- ts(AirPassengers, frequency=12)
-plot(passengers_ts, main=AirPassengers)
-arima_model <- auto.arima(passengers_ts)
-summary(arima_model)
-
-forecast_plot <- forecast(arima_model, h=24)
-plot(forecast_plot, main="Forecast for airline passengers")
+passengers_ts <- ts(AirPassengers, frequency = 12)
+plot(passengers_ts, main = "Time Series Plot")
+decomposed <- decompose(passengers_ts)
+par(mfrow = c(4, 1))
+plot(decomposed)
+acf(passengers_ts, main = "ACF")
+pacf(passengers_ts, main = "PACF")
